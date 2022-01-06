52438c0f12f26d5891b76c1579291aa13f4a501a
Tema Zinkin <commit@azinkin.ru>
2022-01-07 02:52:35 +0300

    urls config

---
+++ quiz/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from quiz.views import (
+    index_view,
+    category_view,
+    quiz_view,
+)
+
+urlpatterns = [
+    path('', index_view, name='index'),
+    path('<slug:category_slug>/', category_view, name='category'),
+    path('<slug:category_slug>/<slug:quiz_slug>/', quiz_view, name='quiz'),
+]
